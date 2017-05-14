3be173775c47a99143a048b0284e6eeb2383ad29
Shane Becker <veganstraightedge@gmail.com>
2017-05-14 11:17:47 -0700

    Add vimeo template and specs

--- spec/expanded_embed_vimeo_spec.rb
+++ spec/expanded_embed_vimeo_spec.rb
@@ -1,44 +1,44 @@
 require "spec_helper"
 
 RSpec.describe MarkdownMedia do
-  describe "#expanded_embed for image" do
-    it "finds an image embed" do
-      result = MarkdownMedia.expanded_embed("http://example.com/photo.png")
+  describe "#expanded_embed for vimeo" do
+    it "finds an vimeo embed" do
+      result = MarkdownMedia.expanded_embed("https://vimeo.com/116239463")
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /></figure>}
+      expected_result = %q{<figure class="video-container" id=""><iframe src="https://player.vimeo.com/video/116239463?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></figure>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption" do
+    it "finds an vimeo embed with a caption" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption"
+        "https://vimeo.com/116239463",
+        caption: "vimeo caption"
       )
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<figure class="video-container" id=""><iframe src="https://player.vimeo.com/video/116239463?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><figcaption class="caption video-caption video-caption-vimeo">vimeo caption</figcaption></figure>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption and link" do
+    it "finds an vimeo embed with a caption and link" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "https://vimeo.com/116239463",
+        caption: "vimeo caption",
         link:    "http://example.com/linked-destination"
       )
 
-      expected_result = %q{<figure id=""><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<figure class="video-container" id=""><iframe src="https://player.vimeo.com/video/116239463?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><figcaption class="caption video-caption video-caption-vimeo">vimeo caption</figcaption></figure>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption, link and id" do
+    it "finds an vimeo embed with a caption, link and id" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "https://vimeo.com/116239463",
+        caption: "vimeo caption",
         link:    "http://example.com/linked-destination",
-        id:      "photo"
+        id:      "vimeo"
       )
 
-      expected_result = %q{<figure id="photo"><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<figure class="video-container" id="vimeo"><iframe src="https://player.vimeo.com/video/116239463?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><figcaption class="caption video-caption video-caption-vimeo">vimeo caption</figcaption></figure>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
   end
