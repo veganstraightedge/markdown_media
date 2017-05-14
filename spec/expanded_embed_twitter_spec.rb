c223bd678add34a2c311adaecb604bf56f39775e
Shane Becker <veganstraightedge@gmail.com>
2017-05-14 11:07:24 -0700

    Add tweet template and specs

--- spec/expanded_embed_twitter_spec.rb
+++ spec/expanded_embed_twitter_spec.rb
@@ -1,44 +1,44 @@
 require "spec_helper"
 
 RSpec.describe MarkdownMedia do
-  describe "#expanded_embed for image" do
-    it "finds an image embed" do
-      result = MarkdownMedia.expanded_embed("http://example.com/photo.png")
+  describe "#expanded_embed for tweet" do
+    it "finds an tweet embed" do
+      result = MarkdownMedia.expanded_embed("https://twitter.com/veganstraightedge/status/863151253911511040")
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /></figure>}
+      expected_result = %q{<blockquote class="twitter-tweet" data-lang="en" id=""><a href="https://twitter.com/veganstraightedge/status/863151253911511040">https://twitter.com/veganstraightedge/status/863151253911511040</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption" do
+    it "finds an tweet embed with a caption" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption"
+        "https://twitter.com/veganstraightedge/status/863151253911511040",
+        caption: "tweet caption"
       )
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<blockquote class="twitter-tweet" data-lang="en" id=""><a href="https://twitter.com/veganstraightedge/status/863151253911511040">https://twitter.com/veganstraightedge/status/863151253911511040</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption and link" do
+    it "finds an tweet embed with a caption and link" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "https://twitter.com/veganstraightedge/status/863151253911511040",
+        caption: "tweet caption",
         link:    "http://example.com/linked-destination"
       )
 
-      expected_result = %q{<figure id=""><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<blockquote class="twitter-tweet" data-lang="en" id=""><a href="https://twitter.com/veganstraightedge/status/863151253911511040">https://twitter.com/veganstraightedge/status/863151253911511040</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption, link and id" do
+    it "finds an tweet embed with a caption, link and id" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "https://twitter.com/veganstraightedge/status/863151253911511040",
+        caption: "tweet caption",
         link:    "http://example.com/linked-destination",
-        id:      "photo"
+        id:      "tweet"
       )
 
-      expected_result = %q{<figure id="photo"><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<blockquote class="twitter-tweet" data-lang="en" id="tweet"><a href="https://twitter.com/veganstraightedge/status/863151253911511040">https://twitter.com/veganstraightedge/status/863151253911511040</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
   end
