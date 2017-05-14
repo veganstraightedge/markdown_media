ccf82b4d18dc24f94928069e4af829b29a8b4740
Shane Becker <veganstraightedge@gmail.com>
2017-05-13 17:27:44 -0700

    Add [[]] pattern matching to parse()

--- lib/markdown_media.rb
+++ lib/markdown_media.rb
@@ -1,12 +1,32 @@
 require "markdown_media/version"
 
 module MarkdownMedia
+  EMBED_REGEX = /\[\[\s*(http[^\]\s]+(?:\s.+)?)\s*\]\]/
+
   class << self
     def parse(text)
-      output = text
+      output = text.gsub(EMBED_REGEX) do |match|
+        embed_tag = $1
+
+        unless embed_tag.to_s.empty?
+          embed_tag_pieces = embed_tag.split(" ")
+
+          url     = embed_tag_pieces.shift
+          # id      = remove_id(embed_tag_pieces)
+          link    = if embed_tag_pieces.to_s.empty?
+            embed_tag_pieces.pop if url_or_path?(embed_tag_pieces.last)
+          end
+          caption = embed_tag_pieces.join(" ")
+
+          # expanded_embed(url, caption: caption, link: link)
+          # , id: id
+
+          [url, caption, link]
+        end
+      end
+
       output
     end
-
 
     def url_or_path?(string)
       string =~ /^(http|\/)\S+/ ? true : false
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -1,11 +1,25 @@
 require "spec_helper"
 
 RSpec.describe MarkdownMedia do
+  describe "EMBED_REGEX constant" do
+    it "is not nil" do
+      expect(MarkdownMedia::EMBED_REGEX).to_not eq(nil)
+    end
+  end
+
   describe "#parse" do
     it "returns a string" do
       result = MarkdownMedia.parse("lorem ipsum")
 
       expect(result).to eq("lorem ipsum")
+    end
+
+    it 'matches [[]] syntax patterns' do
+      result = MarkdownMedia.parse("lorem ipsum [[http://example.com/photo.png]] dolor sit")
+
+      expected_output = "lorem ipsum " + ["http://example.com/photo.png", "", nil].to_s + " dolor sit"
+
+      expect(result).to eq(expected_output)
     end
   end
 end
