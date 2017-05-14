7796261959a950e4e00084c5927c359d39cbe63c
Shane Becker <veganstraightedge@gmail.com>
2017-05-14 11:02:28 -0700

    Add link template and specs

--- spec/expanded_embed_link_spec.rb
+++ spec/expanded_embed_link_spec.rb
@@ -1,44 +1,44 @@
 require "spec_helper"
 
 RSpec.describe MarkdownMedia do
-  describe "#expanded_embed for image" do
-    it "finds an image embed" do
-      result = MarkdownMedia.expanded_embed("http://example.com/photo.png")
+  describe "#expanded_embed for link" do
+    it "finds an link embed" do
+      result = MarkdownMedia.expanded_embed("http://example.com/something/unknown")
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /></figure>}
+      expected_result = %q{<a href="http://example.com/something/unknown" id="">example.com/something/unknown</a>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption" do
+    it "finds an link embed with a caption" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption"
+        "http://example.com/something/unknown",
+        caption: "link caption"
       )
 
-      expected_result = %q{<figure id=""><img src="http://example.com/photo.png" /><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<a href="http://example.com/something/unknown" id="">link caption</a>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption and link" do
+    it "finds an link embed with a caption and link" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "http://example.com/something/unknown",
+        caption: "link caption",
         link:    "http://example.com/linked-destination"
       )
 
-      expected_result = %q{<figure id=""><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<a href="http://example.com/something/unknown" id="">link caption</a>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
 
-    it "finds an image embed with a caption, link and id" do
+    it "finds an link embed with a caption, link and id" do
       result = MarkdownMedia.expanded_embed(
-        "http://example.com/photo.png",
-        caption: "photo caption",
+        "http://example.com/something/unknown",
+        caption: "link caption",
         link:    "http://example.com/linked-destination",
-        id:      "photo"
+        id:      "link"
       )
 
-      expected_result = %q{<figure id="photo"><a href="http://example.com/linked-destination"><img src="http://example.com/photo.png" /></a><figcaption>photo caption</figcaption></figure>}
+      expected_result = %q{<a href="http://example.com/something/unknown" id="link">link caption</a>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
   end
