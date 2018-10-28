dd519f0972834240f210bb7f7e325b36648b5017
Shane Becker <veganstraightedge@gmail.com>
2018-10-28 00:17:13 -0700

    Remove absolute positioning on Giphy iframe

--- spec/expanded_embed_giphy_spec.rb
+++ spec/expanded_embed_giphy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe MarkdownMedia do
     it "finds a giphy embed" do
       result = MarkdownMedia.expanded_embed("https://giphy.com/gifs/dog-animal-friendship-4k5gSMvd1yr60")
 
-      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" style="position:absolute" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div></figure>}
+      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div></figure>}
 
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
@@ -16,7 +16,7 @@ RSpec.describe MarkdownMedia do
         caption: "giphy gif caption"
       )
 
-      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" style="position:absolute" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
+      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
 
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
@@ -28,7 +28,7 @@ RSpec.describe MarkdownMedia do
         link: "http://example.com/linked-destination"
       )
 
-      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" style="position:absolute" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
+      expected_result = %q{<figure class="video-container " id=""><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
 
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
@@ -41,7 +41,7 @@ RSpec.describe MarkdownMedia do
         id: 'giphy-content-id'
       )
 
-      expected_result = %q{<figure class="video-container " id="giphy-content-id"><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" style="position:absolute" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
+      expected_result = %q{<figure class="video-container " id="giphy-content-id"><div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/4k5gSMvd1yr60" width="100%" height="100%" frameborder="0" class="giphy-embed" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe></div><figcaption class="caption video-caption video-caption-giphy"><p>giphy gif caption</p></figcaption></figure>}
       expect(strip_html_whitespace(result)).to eq(expected_result)
     end
   end
