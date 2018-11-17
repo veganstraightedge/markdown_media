fae5e4b331a9b564982b7e691cbe8d29c9c74715
Shane Becker <veganstraightedge@gmail.com>
2018-11-17 12:35:51 -0800

    Extract content pieces and URL

--- lib/source_code.rb
+++ lib/source_code.rb
@@ -1,9 +1,15 @@
 class SourceCode
-  attr_reader :content
+  attr_reader :content, :url, :pieces
+
+  # URL, then a space, then optional caption/id/class/link
+  EMBED_REGEX = /(http[^\]\s]+(?:\s.+)?)/.freeze
 
   def initialize content:
     @content = content
     strip_brackets!
+    validate_url_presence
+    split_content_into_pieces!
+    extract_url!
   end
 
   private
@@ -11,4 +17,22 @@ class SourceCode
   def strip_brackets!
     @content.gsub!('[[', '').gsub!(']]', '').strip!
   end
+
+  def validate_url_presence
+    matched_content = content.match(EMBED_REGEX)
+    raise TypeError, "Expected to start with a URL" if matched_content.to_s.empty?
+  end
+
+  def split_content_into_pieces!
+    matched_content = content.match(EMBED_REGEX)
+
+    matched_content = matched_content[1]
+
+    @pieces = matched_content.split
+  end
+
+  def extract_url!
+    @url = @pieces.shift
+  end
+
 end
--- spec/source_code_spec.rb
+++ spec/source_code_spec.rb
@@ -1,21 +1,37 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe SourceCode do
-  describe "#initialize" do
+  describe '#initialize' do
     it 'requires a content parameter' do
       expect { SourceCode.new }.to raise_error(ArgumentError)
     end
 
     it 'is valid with required content parameter' do
-      expect { SourceCode.new(content: '[[ test ]]') }.to_not raise_error
+      expect { SourceCode.new(content: '[[ http://example.com/image.png ]]') }.to_not raise_error
     end
 
     it 'sets @content as readable' do
-      expect(SourceCode.new(content: '[[ test ]]').content).to be_truthy
+      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').content).to be_truthy
     end
 
     it 'strips wrapping double brackets' do
-      expect(SourceCode.new(content: '[[ test ]]').content).to eq 'test'
+      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').content).to eq 'http://example.com/image.png'
+    end
+
+    it 'sets @url as readable' do
+      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').url).to be_truthy
+    end
+
+    it 'extracts @url from content' do
+      expect(SourceCode.new(content: '[[ http://example.com/image.png caption ]]').url).to eq 'http://example.com/image.png'
+    end
+
+    it 'sets @pieces as readable' do
+      expect(SourceCode.new(content: '[[ http://example.com/image.png caption ]]').pieces).to be_truthy
+    end
+
+    xit 'must start with a URL' do
+      expect(SourceCode.new(content: '[[ caption ]]')).to raise_error(TypeError)
     end
   end
 end
