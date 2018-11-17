a5b007c3aba98448df9a117e00183167638d7223
Shane Becker <veganstraightedge@gmail.com>
2018-11-17 12:40:02 -0800

    DRY SourceCode specs a bit

--- spec/source_code_spec.rb
+++ spec/source_code_spec.rb
@@ -2,32 +2,35 @@ require 'spec_helper'
 
 RSpec.describe SourceCode do
   describe '#initialize' do
+    simple_image_embed           = SourceCode.new(content: '[[ http://example.com/image.png ]]')
+    simple_captioned_image_embed = SourceCode.new(content: '[[ http://example.com/image.png caption ]]')
+
     it 'requires a content parameter' do
       expect { SourceCode.new }.to raise_error(ArgumentError)
     end
 
     it 'is valid with required content parameter' do
-      expect { SourceCode.new(content: '[[ http://example.com/image.png ]]') }.to_not raise_error
+      expect { simple_image_embed }.to_not raise_error
     end
 
     it 'sets @content as readable' do
-      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').content).to be_truthy
+      expect(simple_image_embed.content).to be_truthy
     end
 
     it 'strips wrapping double brackets' do
-      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').content).to eq 'http://example.com/image.png'
+      expect(simple_image_embed.content).to eq 'http://example.com/image.png'
     end
 
     it 'sets @url as readable' do
-      expect(SourceCode.new(content: '[[ http://example.com/image.png ]]').url).to be_truthy
+      expect(simple_image_embed.url).to be_truthy
     end
 
     it 'extracts @url from content' do
-      expect(SourceCode.new(content: '[[ http://example.com/image.png caption ]]').url).to eq 'http://example.com/image.png'
+      expect(simple_captioned_image_embed.url).to eq 'http://example.com/image.png'
     end
 
     it 'sets @pieces as readable' do
-      expect(SourceCode.new(content: '[[ http://example.com/image.png caption ]]').pieces).to be_truthy
+      expect(simple_captioned_image_embed.pieces).to be_truthy
     end
 
     xit 'must start with a URL' do
