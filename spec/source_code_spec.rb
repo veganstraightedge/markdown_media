8c75174523f735bf2c95dbf1c895de0133079bb7
Shane Becker <veganstraightedge@gmail.com>
2018-11-21 15:39:37 -0800

    Organize specs

--- spec/source_code_spec.rb
+++ spec/source_code_spec.rb
@@ -2,49 +2,55 @@ require 'spec_helper'
 
 RSpec.describe SourceCode do
   describe '#initialize' do
-    simple_embed           = SourceCode.new(content: '[[ http://example.com/image.png ]]')
-    simple_captioned_embed = SourceCode.new(content: '[[ http://example.com/image.png caption ]]')
-    simple_ided_embed      = SourceCode.new(content: '[[ http://example.com/image.png id:test ]]')
+    simple_embed              = SourceCode.new(content: '[[ http://example.com/image.png ]]')
+    simple_embed_with_caption = SourceCode.new(content: '[[ http://example.com/image.png caption ]]')
+    simple_embed_with_id      = SourceCode.new(content: '[[ http://example.com/image.png id:test ]]')
+    simple_embed_with_url     = SourceCode.new(content: '[[ http://example.com/image.png http://example.org ]]')
 
-    it 'requires a content parameter' do
-      expect { SourceCode.new }.to raise_error(ArgumentError)
+    context '#content' do
+      it 'requires a content parameter' do
+        expect { SourceCode.new }.to raise_error(ArgumentError)
+      end
+
+      it 'is valid with required content parameter' do
+        expect { simple_embed }.to_not raise_error
+      end
+
+      it 'sets @content as readable' do
+        expect(simple_embed.content).to be_truthy
+      end
     end
 
-    it 'is valid with required content parameter' do
-      expect { simple_embed }.to_not raise_error
+    context 'strips brackets' do
+      it 'strips wrapping double brackets' do
+        expect(simple_embed.content).to eq 'http://example.com/image.png'
+      end
     end
 
-    it 'sets @content as readable' do
-      expect(simple_embed.content).to be_truthy
+    context '#pieces' do
+      it 'sets @pieces as readable' do
+        expect(simple_embed_with_caption.pieces).to be_truthy
+      end
     end
 
-    it 'strips wrapping double brackets' do
-      expect(simple_embed.content).to eq 'http://example.com/image.png'
+    context 'with URL' do
+      it 'sets @url as readable' do
+        expect(simple_embed.url).to be_truthy
+      end
+
+      it 'extracts @url from content' do
+        expect(simple_embed_with_caption.url).to eq 'http://example.com/image.png'
+      end
+
+      xit 'must start with a URL' do
+        expect(SourceCode.new(content: '[[ caption ]]')).to raise_error(TypeError)
+      end
     end
 
-    it 'sets @url as readable' do
-      expect(simple_embed.url).to be_truthy
+    context 'with ID' do
+      it 'sets @id as readable' do
+        expect(simple_embed_with_id.id).to be_truthy
+      end
     end
-
-    it 'extracts @url from content' do
-      expect(simple_captioned_embed.url).to eq 'http://example.com/image.png'
-    end
-
-    it 'sets @pieces as readable' do
-      expect(simple_captioned_embed.pieces).to be_truthy
-    end
-
-    xit 'must start with a URL' do
-      expect(SourceCode.new(content: '[[ caption ]]')).to raise_error(TypeError)
-    end
-
-    it 'sets @id as readable' do
-      expect(simple_ided_embed.id).to be_truthy
-    end
-
-    it 'extracts @url from content' do
-      expect(simple_captioned_embed.url).to eq 'http://example.com/image.png'
-    end
-
   end
 end
